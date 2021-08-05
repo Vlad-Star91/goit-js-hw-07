59aaa2fd75a46a5d4fb4d3367012d8cc1f97fcf9
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-08-05 21:44:30 +0200

    Done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientElem = ingredients.map(ingredient => {
+  const ingredientElem = document.createElement('li')
+  console.log(ingredientElem);
+  ingredientElem.textContent = ingredient;
+
+  return ingredientElem;
+})
+
+const ingredientElemList = document.querySelector('#ingredients')
+ingredientElemList.append(...ingredientElem)
