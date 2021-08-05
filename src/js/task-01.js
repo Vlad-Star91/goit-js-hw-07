f764eb4e49b2933a5e064e24d961ba02b0fb2eb6
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-08-05 21:29:13 +0200

    Done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,15 @@
-const categoriesElemList = document.querySelector('#categories')
-console.log(categoriesElemList);
+const categoriesElem = document.querySelector('#categories')
+console.log(categoriesElem);
 
+const elemItems = document.querySelectorAll('.item')
+console.log(`В списке ${elemItems.length} категории`);
 
+const titles = []
+elemItems.forEach(elem => {
+    console.log('Категория:', elem.getElementsByTagName('h2')[0].textContent);
+    console.log('Количество элементов:', elem.getElementsByTagName('li').length);
+    console.log('');
+});
+ 
 
 
