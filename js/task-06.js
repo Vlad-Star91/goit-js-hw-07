7e821a61cbb1444463011384a264ab4c96b4b3d5
Vladyslav Starodubets <dp181191svn@gmail.com>
2021-08-10 11:44:18 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,12 @@
 const inputValidationElem = document.querySelector("#validation-input");
 const dataLengthElem = Number(inputValidationElem.dataset.length);
 
+inputValidationElem.addEventListener('blur', checkDataLength)
+
 function checkDataLength(event) {
-    if (event.currentTarget.value.length === dataLength) {
+    if (event.currentTarget.value.length === dataLengthElem) {
         event.currentTarget.classList.add("valid");
     } else {
          event.currentTarget.classList.add("invalid");
     }
 }
-inputValidationElem.addEventListener('blur', checkDataLength)
